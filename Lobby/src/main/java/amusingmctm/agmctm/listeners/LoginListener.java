be0461466593e4548bb4443fb0b48a82fe9cd240
minnminn <toikenosa@yahoo.co.jp>
2019-07-14 11:29:41 +0900

    Join Sound add

--- Lobby/src/main/java/amusingmctm/agmctm/listeners/LoginListener.java
+++ Lobby/src/main/java/amusingmctm/agmctm/listeners/LoginListener.java
@@ -1,8 +1,11 @@
 package amusingmctm.agmctm.listeners;
 
 import amusingmctm.agmctm.Main;
-
+;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -19,6 +22,11 @@ public class LoginListener implements Listener {
         String setConfig = plugin.getConfig().getString("JoinMs");
         setConfig = setConfig.replaceAll("%player", event.getPlayer().getName());
         event.setJoinMessage(ChatColor.translateAlternateColorCodes(('&'), setConfig));
+
+        for(Player player : Bukkit.getOnlinePlayers()){
+            player.playSound(player.getLocation(), Sound.DOOR_OPEN,10,0);
+        }
+
     }
 
 }
